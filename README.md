33672dfa17f3ac717d40a130d97fd62b7ad3b76e
tgnc <tgnc@yandex-team.ru>
2020-02-08 00:49:54 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Little promo =)
+
+## Prerequisites
+* node 10+
+* yarn
+
+## Development 
+Run in dev mode: `yarn run dev`
---
+++ server/app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+
+
+const publicPath = path.join(__dirname, '../..', 'public');
+const port = process.env.PORT || 5000;
+
+const app = express()
+    .use(express.static(publicPath))
+    .get('/api/*', (req, res) => {res.json({test: 'api'})})
+    .get('*', (req, res) => {
+        res.sendFile(path.join(publicPath, 'index.html'));
+    });
+
+app.listen(port, listenCallback);
+
+function listenCallback(err) {
+    if (err) {
+        console.error('Application start error ', err);
+    }
+    console.info(`Application started on port ${port}`);
+}
