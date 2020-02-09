95144b0722ccfc8c430fc19912f91aaabab29f5e
tgnc <tgnc@yandex-team.ru>
2020-02-09 20:57:17 +0300

    updated travis and node

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../..', 'public');
+const staticPath = path.join(__dirname, "public", "static");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(staticPath));
 // const app = express()
 //     .use(express.static(publicPath))
 //     .get('/api/*', (req, res) => {res.json({test: 'api'})})
