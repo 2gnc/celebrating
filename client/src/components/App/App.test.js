b6f44aaf05fa503db8609237c8845b626f26451b
tgnc <tgnc@yandex-team.ru>
2020-02-10 13:33:33 +0300

    changed structure

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = getByText(/hello/i);
+  expect(el).toBeInTheDocument();
 });
