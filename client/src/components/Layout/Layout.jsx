acc067b373864a7f1f77ecf327f2190ca355747f
tgnc <tgnc@yandex-team.ru>
2020-02-11 19:17:02 +0300

    mobile detection and some styles

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -5,26 +5,30 @@ import TerminalContainer from '../Terminal/TerminalContainer';
 import HeaderContainer from '../Header/HeaderContainer';
 import Preview from '../Preview/Preview';
 
-function Layout({isPreviewShown}) {
-    return (
-        <div className='layout'>
-            <div className='layout__tools'>
-                <ToolsContainer />
-            </div>
-            <div className='layout__main'>
-                <div className='layout__box'>
-                    <main className={`layout__code layout__code_preview_${isPreviewShown}`}>
-                        <HeaderContainer />
-                        main
-                    </main>
-                    <aside className={`layout__preview layout__preview_preview_${isPreviewShown}`}>
-                        <Preview />
-                    </aside>
-                </div>
-                <TerminalContainer />
-            </div>
-        </div>
-    );
-}
+export default class Layout extends React.Component {
+    componentDidMount = () => {
+        this.props.isMobile && this.props.onMobileDetected();
+    }
 
-export default Layout;
+    render() {
+        return (
+            <div className={`layout layout_mob_${this.props.isMobile}`}>
+                <div className='layout__tools'>
+                    <ToolsContainer />
+                </div>
+                <div className='layout__main'>
+                    <div className='layout__box'>
+                        <main className={`layout__code layout__code_preview_${this.props.isPreviewShown}`}>
+                            <HeaderContainer />
+                            main
+                        </main>
+                        <aside className={`layout__preview layout__preview_preview_${this.props.isPreviewShown}`}>
+                            <Preview />
+                        </aside>
+                    </div>
+                    <TerminalContainer />
+                </div>
+            </div>
+        );
+    }
+}
