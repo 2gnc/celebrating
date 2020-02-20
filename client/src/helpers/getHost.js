9e3eaaa99674240e98c8b37ee0f0d0f0f6e617d1
tgnc <tgnc@yandex-team.ru>
2020-02-20 23:41:00 +0300

    experiment

--- client/src/helpers/getHost.js
+++ client/src/helpers/getHost.js
@@ -1,1 +1,1 @@
-export const host = process.env.NODE_ENV === 'development' ? 'ws://localhost:9797' : 'wss://celebration-2020.herokuapp.com:9797';
+export const host = process.env.NODE_ENV === 'development' ? 'ws://localhost:5555' : window.location.origin.replace(/^http/, 'ws');
--- server/app.js
+++ server/app.js
@@ -3,7 +3,29 @@ const express = require('express');
 const wakeUp = require('./utils/wake-up');
 const {v1Router} = require('./v1');
 require('./db/setup');
-require('./db/socket');
+// require('./db/socket');
+
+const WebSocket = require('ws');
+const http = require('http');
+const {prepareLog} = require('./utils/prepare-log');
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+wss.on('connection', function (ws) {
+    ws.on('message', async function (message) {
+        for (const client of wss.clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                const log = await prepareLog();
+                const logMessage = {};
+                logMessage.type = 'UPDATE_LOG';
+                logMessage.message = log[0][1].message;
+                client.send(message);
+                client.send(JSON.stringify(logMessage));
+            }
+        }
+    });
+});
 
 const port = process.env.PORT || 5555;
 const isProd = process.env.NODE_ENV === 'production';
@@ -11,14 +33,15 @@ const URL = 'https://celebration-2020.herokuapp.com/';
 
 const publicPath = isProd ? path.join(__dirname, '..', 'public') : path.join(__dirname, '..', 'client', 'public');
 
-const app = express()
+// const app = express()
+app
 .use(express.static(publicPath))
 .use('/v1', v1Router)
 .get('/', (_req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, listenCallback);
+server.listen(port, listenCallback);
 
 function listenCallback(err) {
     if (err) {
