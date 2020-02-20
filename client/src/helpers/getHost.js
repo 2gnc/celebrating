f1bbe306dd9d1e0b8c9e7bd7dab6023c277210f7
tgnc <tgnc@yandex-team.ru>
2020-02-20 22:52:35 +0300

    ports changed in prod

--- client/src/helpers/getHost.js
+++ client/src/helpers/getHost.js
@@ -1,1 +1,1 @@
-export const host = process.env.NODE_ENV === 'development' ? 'ws://localhost:8080' : 'wss://celebration-2020.herokuapp.com:8080';
+export const host = process.env.NODE_ENV === 'development' ? 'ws://localhost:8080' : 'wss://celebration-2020.herokuapp.com';
