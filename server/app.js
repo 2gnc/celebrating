77b39103f7c0f8910f45138253cba6131c91226b
tgnc <tgnc@yandex-team.ru>
2020-02-21 11:17:59 +0300

    fixed logs dublication

--- server/app.js
+++ server/app.js
@@ -15,12 +15,14 @@ wss.on('connection', function (ws) {
     ws.on('message', async function (message) {
         for (const client of wss.clients) {
             if (client.readyState === WebSocket.OPEN) {
-                const log = await prepareLog();
-                const logMessage = {};
-                logMessage.type = 'UPDATE_LOG';
-                logMessage.message = log[0][1].message;
                 client.send(message);
-                client.send(JSON.stringify(logMessage));
+                if (JSON.parse(message).type !== 'STARTED_ANSWER_CHECK') {
+                    const log = await prepareLog();
+                    const logMessage = {};
+                    logMessage.type = 'UPDATE_LOG';
+                    logMessage.message = log[0][1].message;
+                    client.send(JSON.stringify(logMessage));
+                }
             }
         }
     });
